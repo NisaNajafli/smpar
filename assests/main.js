4ad3eb60dacb98c2af8b829f8be9fd3e2eaa32bf
Nisa Najafli <ncfliniss@gmail.com>
2026-01-19 15:34:37 +0400

    new

--- assests/main.js
+++ assests/main.js
@@ -20,3 +20,89 @@ navigation: {
             prevEl: "[data-slider-button='1']"
         }
 });
+new Swiper(".saunas-slider", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  loop: true,
+//   autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: false,
+//   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1100: { slidesPerView: 3 }
+  }
+});
+new Swiper(".chans-slider", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  loop: true,
+  //   autoplay: {
+//     delay: 5000,
+//     disableOnInteraction: false,
+//   },
+  navigation: {
+    nextEl: ".chans-section .swiper-button-next",
+    prevEl: ".chans-section .swiper-button-prev",
+  },
+  breakpoints: {
+    0: { slidesPerView: 1 },
+    768: { slidesPerView: 2 },
+    1100: { slidesPerView: 3 }
+  }
+});
+let currentStep = 0;
+const steps = document.querySelectorAll(".quiz-step");
+const progress = document.getElementById("quizProgress");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+const quizButtons = document.querySelector(".quiz-buttons-premium");
+
+function showStep(step) {
+  steps.forEach((el, i) => {
+    el.classList.toggle("active", i === step);
+  });
+
+  // Progress bar
+  const percent = ((step + 1) / steps.length) * 100;
+  progress.style.width = percent + "%";
+
+  // Prev button
+  prevBtn.style.display = step === 0 ? "none" : "inline-block";
+
+  // Next button text
+  nextBtn.textContent = step === steps.length - 2 ? "Отправить" : "Далее";
+
+  // Hide buttons on final success screen
+  if (step === steps.length - 1) {
+    quizButtons.style.display = "none";
+  } else {
+    quizButtons.style.display = "flex";
+  }
+}
+
+nextBtn.addEventListener("click", () => {
+  // If not last input step → go next
+  if (currentStep < steps.length - 2) {
+    currentStep++;
+    showStep(currentStep);
+  } 
+  // If last form step → show success screen
+  else if (currentStep === steps.length - 2) {
+    currentStep++;
+    showStep(currentStep);
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  currentStep--;
+  showStep(currentStep);
+});
+
+// Init
+showStep(currentStep);
